3bb496a2afd1bd79cd5874beff36cb2eec741e50
Lzx667 <1018488753@qq.com>
2022-09-27 15:26:59 +0800

    项目初始化-导入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
+  <div id="app">
     <h2>黑马头条</h2>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <!-- <i class="toutiao toutiao-shanchu"></i> -->
     <router-view></router-view>
   </div>
 </template>
