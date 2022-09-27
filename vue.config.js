b75587bb5df719c6dca394122ad13ca51fe40d8e
Lzx667 <1018488753@qq.com>
2022-09-27 14:39:58 +0800

    项目初始化-调整初始化目录结构

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false, // 关闭eslint语法检测
+  devServer: {
+    // open: true, //自动打开
+    host: 'localhost', // 打开地址
+    port: 2342
+  }
 })
